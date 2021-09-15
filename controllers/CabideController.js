49a29506217e54898deafc980faeb5699687fe69
Daiane <daianeribeiro2@yahoo.com.br>
2021-09-14 22:58:11 -0300

    Página de Login

--- controllers/CabideController.js
+++ controllers/CabideController.js
@@ -1,5 +1,8 @@
 module.exports = {
     index: (req, res) => {
         res.render('index', { title: 'Cabide das Noivas - Home' });
+      },
+      login: (req, res) => {
+        res.render('login', { title: 'Cabide das Noivas - Login' });
       }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', CabideController.index);
+router.get('/login', CabideController.login);
 
 
 module.exports = router;
